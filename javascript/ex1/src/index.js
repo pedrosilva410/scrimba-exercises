8b6b3c16d37bfd2c22257f15150b10b87923d9ac
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2022-11-04 17:12:54 +0100

    ex 1

--- javascript/ex1/src/index.js
+++ javascript/ex1/src/index.js
@@ -1,9 +1,16 @@
-const counter = document.getElementById('counter');
-const value = counter.value;
-const adder = document.getElementById('increment')
+let counter = 0
+const counterTag = document.getElementById("counterTag")
 
-adder.addEventListener('click', () => {
-	console.log('aaaa');
-	value = value = 1;
-	counter.innerHTML = value;
-});
+document.getElementById("add").addEventListener("click", adder)
+document.getElementById("save").addEventListener("click", saver)
+
+function adder() {
+	counter += 1;
+	counterTag.innerHTML = counter
+}
+
+function saver() {
+	document.getElementById("entries").innerHTML = entries.innerHTML + counter + " - "
+	counter = 0;
+	counterTag.innerHTML = 0;
+};
