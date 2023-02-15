0444644d5ea8c4784d0ea741a6884cbf23e8d3b2
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2023-02-15 16:01:53 +0100

    finished the design

--- javascript/ex3/src/index.js
+++ javascript/ex3/src/index.js
@@ -1,11 +1,13 @@
 const homeScore = document.getElementById("homeScore");
 const guestScore = document.getElementById("guestScore");
 
-document.getElementById("addHome1").addEventListener("click", ()=>addScore(1));
-document.getElementById("addHome2").addEventListener("click", ()=>addScore(2));
-document.getElementById("addHome3").addEventListener("click", ()=>addScore(3));
-document.getElementById("addGuest1").addEventListener("click", ()=>addScore(1, guestScore));
-document.getElementById("addGuest2").addEventListener("click", ()=>addScore(2, guestScore));
-document.getElementById("addGuest3").addEventListener("click", ()=>addScore(3, guestScore));
+document.getElementById("addHome1").addEventListener("click", () => addScore(1));
+document.getElementById("addHome2").addEventListener("click", () => addScore(2));
+document.getElementById("addHome3").addEventListener("click", () => addScore(3));
+document.getElementById("addGuest1").addEventListener("click", () => addScore(1, guestScore));
+document.getElementById("addGuest2").addEventListener("click", () => addScore(2, guestScore));
+document.getElementById("addGuest3").addEventListener("click", () => addScore(3, guestScore));
 
-function addScore(point, team=homeScore){team.innerHTML= Number(team.innerHTML) + point;}
+function addScore(point, team = homeScore) {
+    team.innerHTML = Number(team.innerHTML) + point;
+}
