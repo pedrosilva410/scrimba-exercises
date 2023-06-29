83c4fc15c0e26095e18ec43ca419c5dc37a5e3ae
Pedro <pedrogdsilva410@gmail.com>
2023-06-29 11:37:21 +0200

    exercise

--- javascript/ex4/src/index.js
+++ javascript/ex4/src/index.js
@@ -1,9 +1,17 @@
 const startGameButton = document.getElementById("startGame");
+const newCardButton = document.getElementById("newCard")
 const messageEl = document.getElementById("message-el");
 const sumEl = document.getElementById("sum-el")
 const cardsEl = document.getElementById("cards-el")
+let firstCard = 0
+let secondCard = 0
+let sum = 0
+let message = "";
+let hasBlackJack = false;
+let isAlive = true;
 
 startGameButton.addEventListener("click", startGame);
+newCardButton.addEventListener("click", drawCard)
 
 
 function randomIntFromInterval(min, max) { // min and max included 
@@ -11,14 +19,15 @@ function randomIntFromInterval(min, max) { // min and max included
 }
 
 function startGame() {
-	const firstCard = randomIntFromInterval(2, 11);
-	const secondCard = randomIntFromInterval(2, 11);
-	const sum = firstCard + secondCard;
+	let firstCard = randomIntFromInterval(2, 11);
+	let secondCard = randomIntFromInterval(2, 11);
 	let message = "";
 	let hasBlackJack = false;
 	let isAlive = true;
 
-	console.log(sum);
+	sum = firstCard + secondCard;
+	newCardButton.style.visibility = "visible"
+	startGameButton.textContent = "New Game"
 
 	if (sum < 21) {
 		message = "Do you want to draw a new card?";
@@ -30,11 +39,31 @@ function startGame() {
 		message = "Bad luck! You're out of the game!";
 	}
 
-	// CASH OUT!
 	messageEl.textContent = message
-	sumEl.textContent = "Sum: " + sum
 	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+	sumEl.textContent = "Sum: " + sum
 	console.log("blackjack: " + hasBlackJack)
 	console.log("is Alive: " + isAlive)
 }
 
+function drawCard() {
+	let newCard = randomIntFromInterval(2, 11);
+	sum = sum + newCard
+
+	if (sum < 21) {
+		message = "Do you want to draw a new card?";
+	} else if (sum === 21) {
+		message = "You've got Blackjack!";
+		hasBlackJack = true;
+	} else {
+		isAlive = false;
+		message = "Bad luck! You're out of the game!";
+		newCardButton.style.visibility = "hidden"
+	}
+
+	messageEl.textContent = message
+	cardsEl.textContent = cardsEl.textContent + " " + newCard;
+	sumEl.textContent = "Sum: " + sum
+	console.log("blackjack: " + hasBlackJack)
+	console.log("is Alive: " + isAlive)
+}
